0f92182d34c386b0cf16d21bb55baf44a9fa4dc8
Mohan Sharma <mohan7427@gmail.com>
2023-03-17 04:18:44 +0530

    add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt  install fio
+pip3 install fastapi
+apt update
+pip3 install fastapi
+pip3 install fastapi-utils
+pip3 install uvicorn
+pip3 install sqlalchemy
+pip3 install pymysql
+apt install nodejs
+apt install nmp
+npm install -g @angular/cli
