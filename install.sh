58007e31e3a0d57e92642433218eda043870ae10
Mohan Sharma <mohan7427@gmail.com>
2023-03-17 07:46:16 +0530

    more changes

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
-apt  install fio
-pip3 install fastapi
 apt update
+apt  install fio
+apt install nodejs
+apt install nmp
+apt install docker
+npm install -g @angular/cli
 pip3 install fastapi
 pip3 install fastapi-utils
 pip3 install uvicorn
 pip3 install sqlalchemy
 pip3 install pymysql
-apt install nodejs
-apt install nmp
-npm install -g @angular/cli
+
+cd webapp/frontend/
+npm install
+
+ng server --host <machine-ip> --port 4200
+
+cd ../../API
